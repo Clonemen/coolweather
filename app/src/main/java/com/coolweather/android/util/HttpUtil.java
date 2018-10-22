1181b08fac34781e67775a17e05c91b96bfbbcf6
DYGUI <noodle1996@qq.com>
2018-10-22 22:34:04 +0800

    Three commit

--- app/src/main/java/com/coolweather/android/util/HttpUtil.java
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -1,9 +1,16 @@
 package com.coolweather.android.util;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 /**
  * Created by 27443 on 2018/10/9.
  */
 
 public class HttpUtil {
-    
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
 }
